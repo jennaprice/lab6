c2da29ca162dc22c3dbbe77aeb689994de0a7f56
J.O.Price <notantisocial@users.noreply.github.com>
2017-10-26 21:26:52 -0400

    Updated Comments

--- src/Lab6.java
+++ src/Lab6.java
@@ -11,13 +11,17 @@ import java.util.StringTokenizer;
 public class Lab6 {
 
 	public static void main(String[] args) {
+
 		Scanner scnr = new Scanner(System.in);
 		String userInput = "";
 		String tokenWord = "";
 
 		System.out.println("Please enter a word and it will be translated into pig latin.");
+		// I decided to take in the whole line and then break it into words
 		userInput = scnr.nextLine();
 		StringTokenizer wordMaker = new StringTokenizer(userInput);
+
+		// while there are more word (tokens) continue to adjust
 		while (wordMaker.hasMoreTokens()) {
 			tokenWord = wordMaker.nextToken();
 			System.out.print(translatePigLatin(tokenWord));
@@ -28,6 +32,7 @@ public class Lab6 {
 		scnr.close();
 	}
 
+	// used a switch minimize if else statements
 	public static String translatePigLatin(String word) {
 		String finalWord = word.toLowerCase();
 		String appender = "";
@@ -43,6 +48,7 @@ public class Lab6 {
 		default:
 			appender = finalWord.charAt(0) + "way ";
 		}
+		// used the substring method to remove the first character
 		finalWord = finalWord.substring(1, finalWord.length()) + "-" + appender;
 		return finalWord;
 	}
